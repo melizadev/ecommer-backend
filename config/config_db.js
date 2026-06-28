42f51d6d8ff6e03117bd4bf54b4ac135a5d30c37
mariann <cesinelisa@gmail.com>
2026-06-28 16:01:49 -0500

    refactor: remove dotenv import and configuration from database connection file

--- config/config_db.js
+++ config/config_db.js
@@ -1,6 +1,4 @@
 import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-dotenv.config()
 export const connectDB = async () => {
     try {
         const dbUrl = process.env.MONGO_DB_URL.replace(
