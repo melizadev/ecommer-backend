e29b83caf661c40b60c1296e9c2ccc6b1bb06b50
mariann <cesinelisa@gmail.com>
2026-06-30 01:41:47 -0500

    refactor: update cookie options to use fixed values for secure and sameSite

--- utils/coockieOptions.js
+++ utils/coockieOptions.js
@@ -1,6 +1,6 @@
 export const cookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    secure: true,
+    sameSite: 'none',
     maxAge: 60 * 60 * 1000,
 }
